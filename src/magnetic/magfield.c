153e07607ca59fb731e17e7dee148fe1bf5b115a
Mathias Hoppe <hoppe@chalmers.se>
2017-08-23 11:10:08 +0200

    Improved support for magnetic fields. Temporarily removed HDF5 support

--- src/magnetic/magfield.c
+++ src/magnetic/magfield.c
@@ -11,90 +11,26 @@
 #include "magfield.h"
 #include "sfile.h"
 
-//char *(*magfield_get_string)(hid_t, const char*);
-//double **(*magfield_get_doubles)(hid_t, const char*, hsize_t);
-
 /**
- * Reads a string from the dataset with name "name".
- *
- * fileid: HDF5 file descriptor of file to read
- * name: Name of dataset to load string from
+ * Transpose a matrix
  */
-/*
-char *magfield_get_string(hid_t fileid, const char *name) {
-	char *s;
-	size_t length;
-	hid_t filetype, memtype, dset;
+double **magfield_transpose(double **a, int rows, int cols) {
+	int i, j;
+	double **r = malloc(sizeof(double*)*cols),
+			*p = malloc(sizeof(double)*rows*cols);
 
-	dset = H5Dopen(fileid, name, H5P_DEFAULT);
-	filetype = H5Dget_type(dset);
-	length = H5Tget_size(filetype);
-	memtype = H5Tcopy(H5T_C_S1);
-	H5Tset_size(memtype, length);
-
-	s = malloc(sizeof(char)*(length+1));
-	H5Dread(dset, memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, s);
-	s[length] = 0;
-
-	H5Tclose(filetype);
-	H5Tclose(memtype);
-	H5Dclose(dset);
-
-	return s;
-}
-*/
-/**
- * Reads an array of C doubles from the dataset
- * with name "name". The dimensions of the array are
- * returned in "dims".
- *
- * fileid: HDF5 file descriptor to read from
- * name: Name of dataset to read
- * dims: Array with two elements. Will contain dimensions of returned value on return.
- *
- * Returns a 1-D array (logically 2-D) which contains
- * the data of the dataset.
- */
-/*
-double **magfield_get_doubles(hid_t fileid, const char *name, hsize_t *dims) {
-	double *data, **pointers;
-	hid_t dset, space;
-	int ndims;
-	unsigned int i;
-
-	if (H5Lexists(fileid, name, H5P_DEFAULT) <= 0)
-		return NULL;
-	
-	dset = H5Dopen(fileid, name, H5P_DEFAULT);
-
-	space = H5Dget_space(dset);
-	ndims = H5Sget_simple_extent_dims(space, dims, NULL);
-
-	if (dims == NULL) {
-		data = malloc(sizeof(double)*ndims);
-		pointers = malloc(sizeof(double*));
-		pointers[0] = data;
-	} else if (ndims == 1) {
-		data = malloc(sizeof(double)*dims[0]);
-		pointers = malloc(sizeof(double*));
-		pointers[0] = data;
-	} else {
-		data = malloc(sizeof(double)*dims[0]*dims[1]);
-		pointers = malloc(sizeof(double*)*dims[0]);
-		for (i = 0; i < dims[0]; i++) {
-			pointers[i] = data+(i*dims[1]);
+	for (i = 0; i < cols; i++) {
+		r[i] = p + i*rows;
+		for (j = 0; j < rows; j++) {
+			r[i][j] = a[j][i];
 		}
 	}
-	
-	H5Dread(dset, H5T_IEEE_F64LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
 
-	H5Sclose(space);
-	H5Dclose(dset);
+	free(a[0]);
+	free(a);
 
-	return pointers;
+	return r;
 }
-*/
-
 /**
  * Load magnetic field data from the HDF5
  * file name "filename". The file is expected
@@ -118,37 +54,45 @@ magfield_t *magfield_load(const char *filename, enum sfile_type ftype) {
 	}
 
 	s = sfile_init(ftype);
-	//(*sfile_open)(filename, SFILE_MODE_READ);
-	(*(s->open))(s, filename, SFILE_MODE_READ);
+	s->open(s, filename, SFILE_MODE_READ);
 
-	data->name = (*(s->get_string))(s, "name");
-	data->desc = (*(s->get_string))(s, "desc");
+	data->name = s->get_string(s, "name");
+	data->desc = s->get_string(s, "desc");
 	if (data->name == NULL) {fprintf(stderr, "The magnetic equilibrium data has no name!\n"); exit(-1);}
 	if (data->desc == NULL) {fprintf(stderr, "The magnetic equilibrium data has no description!\n"); exit(-1);}
 
-	temp = (*(s->get_doubles))(s, "maxis", dims);
+	temp = s->get_doubles(s, "maxis", dims);
 	if (temp == NULL) {fprintf(stderr, "The magnetic equilibrium data contains no information about the magnetic axis (maxis)!\n"); exit(-1);}
 	data->axis_r = temp[0][0];
 	data->axis_z = temp[0][1];
 	free(temp);
 
-	temp = (*(s->get_doubles))(s, "r", dims);
+	temp = s->get_doubles(s, "r", dims);
 	if (temp == NULL) {fprintf(stderr, "The magnetic equilibrium data contains no radial grid data!\n"); exit(-1);}
 	data->r = *temp; free(temp);
 	data->nr = dims[1];
-	temp = (*(s->get_doubles))(s, "z", dims);
+	temp = s->get_doubles(s, "z", dims);
 	if (temp == NULL) {fprintf(stderr, "The magnetic equilibrium data contains no vertical grid data!\n"); exit(-1);}
 	data->z = *temp; free(temp);
 	data->nz = dims[1];
 
-	data->Br = (*(s->get_doubles))(s, "Br", dims);
-	data->Bphi=(*(s->get_doubles))(s, "Bphi", dims);
-	data->Bz = (*(s->get_doubles))(s, "Bz", dims);
+	/* Load and, if necessary, transpose the magnetic
+	 * field components. Different file formats tend
+	 * to save the magnetic field in different ways,
+	 * and so sometimes we may have to transpose them.
+	 */
+	data->Br = s->get_doubles(s, "Br", dims);
+	if (dims[0] != (unsigned int)data->nr) data->Br = magfield_transpose(data->Br, dims[0], dims[1]);
+	data->Bphi=s->get_doubles(s, "Bphi", dims);
+	if (dims[0] != (unsigned int)data->nr) data->Bphi = magfield_transpose(data->Bphi, dims[0], dims[1]);
+	data->Bz = s->get_doubles(s, "Bz", dims);
+	if (dims[0] != (unsigned int)data->nr) data->Bz = magfield_transpose(data->Bz, dims[0], dims[1]);
+
 	if (data->Br == NULL) {fprintf(stderr, "The magnetic equilibrium data has no radial component!\n"); exit(-1);}
 	if (data->Bphi==NULL) {fprintf(stderr, "The magnetic equilibrium data has no toroidal component!\n"); exit(-1);}
 	if (data->Bz == NULL) {fprintf(stderr, "The magnetic equilibrium data has no vertical component!\n"); exit(-1);}
 
-	temp = (*(s->get_doubles))(s, "wall", dims);
+	temp = s->get_doubles(s, "wall", dims);
 	if (temp != NULL) {
 		data->nwall = dims[1];
 		data->wall_r = temp[0];
@@ -160,7 +104,7 @@ magfield_t *magfield_load(const char *filename, enum sfile_type ftype) {
 		data->wall_z = NULL;
 	}
 
-	temp = (*(s->get_doubles))(s, "separatrix", dims);
+	temp = s->get_doubles(s, "separatrix", dims);
 	if (temp != NULL) {
 		data->nsep = dims[1];
 		data->sep_r = temp[0];
@@ -171,7 +115,7 @@ magfield_t *magfield_load(const char *filename, enum sfile_type ftype) {
 		data->sep_z = NULL;
 	}
 
-	(*(s->close))(s);
+	s->close(s);
 
 	return data;
 }
